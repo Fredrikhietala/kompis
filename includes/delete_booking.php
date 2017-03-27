c05ed2794d01e520c1436a29c349243db878a9a0
Fredrikhietala <hietala.fredrik@gmail.com>
2017-03-27 22:19:37 +0200

    Moved headers

--- includes/delete_booking.php
+++ includes/delete_booking.php
@@ -6,14 +6,15 @@ if (isset($_POST['idbabysitter'])) {
 	$sqlDelete = "DELETE FROM `bookingbabysitter` WHERE `id` = :idbabysitter";
 	$stmDelete = $pdo->prepare($sqlDelete);
 	$stmDelete->execute(['idbabysitter' => $_POST['idbabysitter']]);
-
-	header("Location ../html/my_page.php");
 }
-	// tar bort bokningen när man trycker på knappen ta bort
+
+header("Location ../html/my_page.php");
+
+// tar bort bokningen när man trycker på knappen ta bort
 if (isset($_POST['idtutor'])) {
 	$sqlDelete = "DELETE FROM `bookingtutor` WHERE `id` = :idtutor";
 	$stmDelete = $pdo->prepare($sqlDelete);
 	$stmDelete->execute(['idtutor' => $_POST['idtutor']]);
-
-	header("Location: ../html/my_page.php");
 }
+
+header("Location: ../html/my_page.php");
