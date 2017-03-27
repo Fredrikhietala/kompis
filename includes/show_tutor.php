68e27c9d51c7ab2f39e8200bfaa5213cdf34643b
Fredrikhietala <hietala.fredrik@gmail.com>
2017-03-27 22:39:32 +0200

    Bind params

--- includes/show_tutor.php
+++ includes/show_tutor.php
@@ -1,13 +1,15 @@
 <?php
 require "config.php";
 
-$startTime = $_GET['starttime'];
-$endTime = $_GET['endtime'];
-// hämtar start- och sluttid från boka barnvakt
-$sql = "SELECT `name`, `startTime`, `endTime` FROM `users` LEFT JOIN `bookingtutor` ON `users`.`name` = `bookingtutor`.`tutor` WHERE `startTime` != '" . $startTime . "' AND `endTime` != '" . $endTime . "' GROUP BY `name`";
+
+// hämtar start- och sluttid från ../html/homework.php
+$sql = "SELECT `name`, `startTime`, `endTime` FROM `users` LEFT JOIN `bookingtutor` ON `users`.`name` = `bookingtutor`.`tutor` WHERE `startTime` != :startTime AND `endTime` != :endTime GROUP BY `name`";
 // visar alla barnvakter som inte är upptagna vald tid
 $stm = $pdo->prepare($sql);
-$stm->execute([]);
+$stm->execute([
+	'startTime' => $_GET['starttime'],
+	'endTime' => $_GET['endtime'],
+	]);
 
 foreach($stm as $row) {
 	echo $row['name'] . "\n";
