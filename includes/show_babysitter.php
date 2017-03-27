79615b9bdf1186eadb94267375283c7c0df6523c
Fredrikhietala <hietala.fredrik@gmail.com>
2017-03-27 22:39:05 +0200

    Bind params

--- includes/show_babysitter.php
+++ includes/show_babysitter.php
@@ -1,15 +1,16 @@
 <?php
 require "config.php";
 
-$startTime = $_GET['starttime'];
-$endTime = $_GET['endtime'];
-// hämtar start- och sluttid från boka barnvakt
-$sql = "SELECT `name`, `startTime`, `endTime` FROM `users` LEFT JOIN `bookingbabysitter` ON `users`.`name` = `bookingbabysitter`.`babysitter` WHERE `startTime` != '" . $startTime . "' AND `endTime` != '" . $endTime . "' GROUP BY `name`";
+
+// hämtar start- och sluttid från ../html/babysitter.php
+$sql = "SELECT `name`, `startTime`, `endTime` FROM `users` LEFT JOIN `bookingbabysitter` ON `users`.`name` = `bookingbabysitter`.`babysitter` WHERE `startTime` != :startTime AND `endTime` != :endTime GROUP BY `name`";
 // visar alla barnvakter som inte är upptagna vald tid
 $stm = $pdo->prepare($sql);
-$stm->execute([]);
+$stm->execute([
+	'startTime' => $_GET['starttime'],
+	'endTime' => $_GET['endtime'],
+	]);
 
 foreach($stm as $row) {
 	echo $row['name'] . "\n";
 }
-?>
